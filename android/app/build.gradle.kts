c449612768fbf6ed1bc052e7fc5ec99742074492
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2025-12-21 17:28:46 +0900

    build(gradle): upgrade source and target compatibility to Java 21
    - Update `sourceCompatibility` and `targetCompatibility` from `JavaVersion.VERSION_11` to `JavaVersion.VERSION_21`.
    - Apply version update across all module-level `build.gradle.kts` files in `android` directory.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -166,8 +166,8 @@ android {
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
 
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     buildFeatures {
         buildConfig = true
